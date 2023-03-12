644ac35d08fad181b29104f2bd767e3f64f2426d
Michael Gaynor <mgaynor228@gmail.com>
2023-03-11 18:08:50 -0600

    updated About Me image

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,11 +6,11 @@ export default function Home(){
    return(
     <div class="mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
 
-			<div class="mb-12">
+			<div class=" mb-12">
 				<h1 class="text-3xl lg:text-4xl text-center mb-6 tracking-wider">About Me</h1>
-				<div class="w-2/5 ml-10 mb-5">
-					<img src= "src/IMG_1369.jpeg" class="ml-44 opacity-75"/>	
-				</div>
+				
+					<img src= "src/IMG_1369.jpeg" class="ml-44 w-2/5 block ml-auto mr-auto mb-8  "/>	
+				
 				<p class="tracking-wide text-sm mb-6 leading-relaxed mx-auto max-w-xl text-center">
 					Hello, my name is Michael Gaynor im a Air Force Veteran, College Graduate and now im a  Software Developer.  <br class="hidden md:block" />
 					Find out how I discovered this new passion in my life !</p>
--- src/Project.jsx
+++ src/Project.jsx
@@ -7,7 +7,7 @@ export default function Project(){
         <div class="mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">   
             <h1 class="text-3xl lg:text-4xl text-center mb-6 tracking-wider pt-5">Projects</h1>
             <div class="flex justify-center -mx-2">	
-                    <div class="w-1/2 p-2">
+                    <div class="w-full p-2">
                         <a href="#" class="block mb-8 bg-black hover:bg-gray-700">
                             <img src="src/InfoMovie.png" href="https://github.com/Mgaynor2022/VSchool-Assignments/tree/main/level%204/capstoneProject/movieApp"
                              class="w-200 h-200 opacity-75" />
@@ -15,11 +15,13 @@ export default function Project(){
                         <a href="https://github.com/Mgaynor2022/VSchool-Assignments/tree/main/level%204/capstoneProject/movieApp" class="block text-xl text-center">InfoMovie App</a>
                         <p class="p-3">Developed app to allow users to extract data from RESTful API regarding movies or Tv-shows that was entered into the search function,
                             and allows users to add their favorite movies or tv-shows to a database.</p>
-                            <div class="flex flex-wrap justify-center p-2 ">
+                            <div class="flex flex-wrap justify-between p-2 ">
                                 <a href="https://github.com/Mgaynor2022/VSchool-Assignments/tree/main/level%204/capstoneProject/movieApp">
                                 <button class="py-3 px-5 mt-5 text-sm text-center text-white bg-black rounded-lg  sm:w-fit hover:border-gray-300  focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">GitHub Link</button>
                                 </a>
+                                <a href="">
                                 <button class="py-3 px-5 mt-5 text-sm text-center text-white bg-black rounded-lg  sm:w-fit hover:border-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">App Link</button>
+                                </a>
                             </div>
                     </div>
                     <div class="w-full p-2 ml-10">
@@ -33,8 +35,11 @@ export default function Project(){
                             dream pet. users to search, filter, and save various animals that are available
                             for adoption all within a local server.  
                         </p>
-                        <div>
-                            <a></a>
+                        <div class="flex flex-wrap justify-between p-2 ">
+                            <a>
+                            <button class="py-3 px-5 mt-5 text-sm text-center text-white bg-black rounded-lg  sm:w-fit hover:border-gray-300  focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">GitHub Link</button>
+                            </a>
+                            <button class="py-3 px-5 mt-5 text-sm text-center text-white bg-black rounded-lg  sm:w-fit hover:border-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">App Link</button>
                         </div>
                     </div>		
                 </div>
