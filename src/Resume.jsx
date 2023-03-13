e263e04cedcc05da1114ff3afe4909eaabb4e7d6
Michael Gaynor <mgaynor228@gmail.com>
2023-03-13 16:35:48 -0500

    fixing images

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -9,9 +9,9 @@ export default function Resume(){
             <div class="flex justify-center -mx-2">	
                     <div class="w-1/2 p-2">
                         <a href="#" class="block mb-8 bg-black hover:bg-gray-700">
-                            <img src="src/ScreeShot Resume.png" class="w-200 h-200 opacity-90" />
+                            <img src="images/ScreeShot Resume.png" class="w-200 h-200 opacity-90" />
                         </a>
-                        <button onClick={"src/ScreeShot Resume.png"}
+                        <button onClick={"images/ScreeShot Resume.png"}
                         class="py-3 px-5 mt-5 text-sm text-center text-white bg-black rounded-lg  sm:w-fit hover:border-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Download Resume</button>
                     </div>	
                 </div>
